6370c4e80d92d72224c11dbbdb9a6a7a73880675
DONGWEONSHIN <147117532+DONGWEONSHIN@users.noreply.github.com>
2023-11-21 19:23:57 +0900

    update

--- edu/yolov8/single.py
+++ edu/yolov8/single.py
@@ -13,11 +13,12 @@ def write_yolov8_txt(folder, annotation):
     f.write("{} {} {} {} {}\n".format(box[0], box[1], box[2], box[3], box[4]))
 
 
-dataPath='C:/Users/park0/highway/labels/'
+#dataPath='C:/Users/park0/highway/labels/'
+dataPath='C:/Users/SBA/my_ws/GoogleCloudAI/edu/yolov8/bbox_highway'
 
 # 파일명을 설정한다.
-xmlFile='Suwon_CH01_20200720_1700_MON_9m_NH_highway_TW5_sunny_FHD.xml'
-
+#xmlFile='Suwon_CH01_20200720_1700_MON_9m_NH_highway_TW5_sunny_FHD.xml'
+xmlFile='Suwon_CH02_20200722_1600_WED_9m_RH_highway_TW5_rainy_FHD.xml'
 # 현재 경로 + 파일명
 label_file = os.path.join(dataPath, xmlFile)
 
